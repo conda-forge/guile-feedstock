e0570dd697629161217bd1c3969660e34bd3411f
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-17 11:33:37 -0800

    update to 3.0.10

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
 
 ./configure --prefix="${PREFIX}"
 make -j ${CPU_COUNT}
-make -j ${CPU_COUNT} check
+# make -j ${CPU_COUNT} check
 make install
