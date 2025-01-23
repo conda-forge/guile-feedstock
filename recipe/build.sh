4c10b3e3a189d2294a281d9fd4bcb874162952a0
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-22 22:10:35 -0800

    fix CPPFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,7 +27,7 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
 fi
 
 ./configure --prefix="${PREFIX}"
-export CFLAGS="${CFLAGS} -DHAVE_GC_IS_HEAP_PTR"
+export CPPFLAGS="${CPPFLAGS:-} -DHAVE_GC_IS_HEAP_PTR"
 make --trace -j ${CPU_COUNT}
 # make --trace -j ${CPU_COUNT} check
 make install
