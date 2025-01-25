26ff1086de7ac50cc579b3c35003247c380c3b1c
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-24 23:24:53 -0800

    add deps and fix cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,7 +15,7 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
     CC=${CC//${CONDA_TOOLCHAIN_HOST}/${CONDA_TOOLCHAIN_BUILD}}
     LD="${LD//${CONDA_TOOLCHAIN_HOST}/${CONDA_TOOLCHAIN_BUILD}}"
 
-    ./configure --host=${BUILD} --prefix="${BUILD_PREFIX}" --with-bdw-gc=${BUILD_PREFIX}/lib/pkgconfig/bdw-gc.pc
+    ./configure --host=${BUILD} --prefix="${BUILD_PREFIX}" --with-bdw-gc=${BUILD_PREFIX}/lib/pkgconfig/bdw-gc.pc || cat config.log
     make --trace -j ${CPU_COUNT}
     make install
     make clean
@@ -30,7 +30,7 @@ fi
 export CPPFLAGS="${CPPFLAGS} -DHAVE_GC_IS_HEAP_PTR -DHAVE_GC_MOVE_DISAPPEARING_LINK"
 ./configure --prefix="${PREFIX}"
 make --trace -j ${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+if [[ "${build_platform}" != "${target_platform}" ]]; then
     # Skip some tests that don't work in CI
     sed -i '/tests\/00-socket.test/d' test-suite/Makefile
     sed -i '/tests\/filesys.test/d' test-suite/Makefile
